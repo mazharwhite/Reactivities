49f8f75bf2eac7b9e344d297570990abbd2e373c
mazharwhite <41548326+mazharwhite@users.noreply.github.com>
2021-05-22 15:41:56 +0500

    store management for activities

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,105 +1,28 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
-import { Container } from "semantic-ui-react";
-import { Activity } from "../models/activity";
-import NavBar from "./NavBar";
-import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
-import { v4 as uuid } from "uuid";
-import agent from "../api/agent";
-import LoadingComponent from "./LoadingComponent";
+import { useEffect } from 'react';
+import { Container } from 'semantic-ui-react';
+import NavBar from './NavBar';
+import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
+import LoadingComponent from './LoadingComponent';
+import { useStore } from '../stores/store';
+import { observer } from 'mobx-react-lite';
 
 function App() {
-  const [activities, setActivities] = useState<Activity[]>([]);
-  const [selectedActivity, setSelectedActivity] =
-    useState<Activity | undefined>(undefined);
-  const [editMode, setEditMode] = useState(false);
-  const [loading, setLoading] = useState(true);
-  const [submitting, setSubmitting] = useState(false);
+	const { activityStore } = useStore();
 
-  useEffect(() => {
-    agent.Activities.list().then((res) => {
-      let activities: Activity[] = [];
-      res.forEach((activity) => {
-        activity.date = activity.date.split("T")[0];
-        activities.push(activity);
-      });
+	useEffect(() => {
+		activityStore.loadActivities();
+	}, [activityStore]);
 
-      setActivities(activities);
-      setLoading(false);
-    });
-  }, []);
+	if (activityStore.loadingInitial) return <LoadingComponent />;
 
-  function handleSelectActivity(id: string) {
-    setSelectedActivity(activities.find((x) => x.id === id));
-  }
-
-  function handleCancelActivity() {
-    setSelectedActivity(undefined);
-  }
-
-  function handleFormOpen(id?: string) {
-    id ? handleSelectActivity(id) : handleCancelActivity();
-
-    setEditMode(true);
-  }
-
-  function handleFormClose() {
-    setEditMode(false);
-  }
-
-  function handleCreateOrEditActivity(activity: Activity) {
-    setSubmitting(true);
-    if (activity.id) {
-      agent.Activities.update(activity).then(() => {
-        setActivities([
-          ...activities.filter((x) => x.id !== activity.id),
-          activity,
-        ]);
-        setSelectedActivity(activity);
-        setEditMode(false);
-        setSubmitting(false);
-      });
-    } else {
-      activity.id = uuid();
-
-      agent.Activities.create(activity).then(() => {
-        setActivities([...activities, activity]);
-        setSelectedActivity(activity);
-        setEditMode(false);
-        setSubmitting(false);
-      });
-    }
-  }
-
-  function handleDeleteActivity(id: string) {
-    setSubmitting(true);
-    agent.Activities.delete(id).then(() => {
-      setActivities([...activities.filter((x) => x.id !== id)]);
-      setSubmitting(false);
-    });
-  }
-
-  if (loading) return <LoadingComponent />;
-
-  return (
-    <>
-      <NavBar openForm={handleFormOpen} />
-      <Container style={{ marginTop: "7rem" }}>
-        <ActivityDashboard
-          activities={activities}
-          selectedActivity={selectedActivity}
-          selectActivity={handleSelectActivity}
-          cancelActivity={handleCancelActivity}
-          editMode={editMode}
-          openForm={handleFormOpen}
-          closeForm={handleFormClose}
-          createOrEdit={handleCreateOrEditActivity}
-          deleteActivity={handleDeleteActivity}
-          submitting={submitting}
-        />
-      </Container>
-    </>
-  );
+	return (
+		<>
+			<NavBar />
+			<Container style={{ marginTop: '7rem' }}>
+				<ActivityDashboard />
+			</Container>
+		</>
+	);
 }
 
-export default App;
+export default observer(App);
--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -1,49 +1,56 @@
-﻿import React, {SyntheticEvent, useState} from "react";
-import {Activity} from "../../../app/models/activity";
-import {Button, Item, Label, Segment} from "semantic-ui-react";
+﻿import { SyntheticEvent, useState } from 'react';
+import { Button, Item, Label, Segment } from 'semantic-ui-react';
+import { useStore } from '../../../app/stores/store';
+import { observer } from 'mobx-react-lite';
 
-interface Props {
-    activities: Activity[];
-    selectActivity: (id: string) => void;
-    deleteActivity: (id: string) => void;
-    submitting: boolean;
-}
+export default observer(function ActivityList() {
+	const [target, setTarget] = useState('');
+	const { activityStore } = useStore();
+	const { activitiesByDate, deleteActivity, loading } = activityStore;
 
-export default function ActivityList({activities, selectActivity, deleteActivity, submitting}: Props) {
+	function handleActivityDelete(
+		e: SyntheticEvent<HTMLButtonElement>,
+		id: string
+	) {
+		setTarget(e.currentTarget.name);
+		deleteActivity(id);
+	}
 
-    const [target, setTarget] = useState('');
-
-    function handleActivityDelete(e: SyntheticEvent<HTMLButtonElement>, id: string) {
-        setTarget(e.currentTarget.name);
-        deleteActivity(id);
-    }
-
-    return (
-        <Segment>
-            <Item.Group divided>
-                {activities.map(activity => (
-                    <Item key={activity.id}>
-                        <Item.Content>
-                            <Item.Header as='a'>{activity.title}</Item.Header>
-                            <Item.Meta>{activity.date}</Item.Meta>
-                            <Item.Description>
-                                <div>{activity.description}</div>
-                                <div>{activity.city}, {activity.venue}</div>
-                            </Item.Description>
-                            <Item.Extra>
-                                <Button onClick={() => selectActivity(activity.id)} floated='right' content='View'
-                                        color='blue'/>
-                                <Button name={activity.id}
-                                        loading={submitting && target === activity.id}
-                                        onClick={(e) => handleActivityDelete(e, activity.id)} floated='right'
-                                        content='Delete'
-                                        color='red'/>
-                                <Label basic content={activity.category}/>
-                            </Item.Extra>
-                        </Item.Content>
-                    </Item>
-                ))}
-            </Item.Group>
-        </Segment>
-    );
-}
+	return (
+		<Segment>
+			<Item.Group divided>
+				{activitiesByDate.map((activity) => (
+					<Item key={activity.id}>
+						<Item.Content>
+							<Item.Header as='a'>{activity.title}</Item.Header>
+							<Item.Meta>{activity.date}</Item.Meta>
+							<Item.Description>
+								<div>{activity.description}</div>
+								<div>
+									{activity.city}, {activity.venue}
+								</div>
+							</Item.Description>
+							<Item.Extra>
+								<Button
+									onClick={() => activityStore.selectActivity(activity.id)}
+									floated='right'
+									content='View'
+									color='blue'
+								/>
+								<Button
+									name={activity.id}
+									loading={loading && target === activity.id}
+									onClick={(e) => handleActivityDelete(e, activity.id)}
+									floated='right'
+									content='Delete'
+									color='red'
+								/>
+								<Label basic content={activity.category} />
+							</Item.Extra>
+						</Item.Content>
+					</Item>
+				))}
+			</Item.Group>
+		</Segment>
+	);
+});
